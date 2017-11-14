b49cbd5799459d6734093652eb7ff3e193fc4b94
Esa <Esa@Esa-PC>
2017-11-14 20:38:15 +0700

    Nilai Mahasiswa

---
+++ src/Model/GajiPegawai.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Esa IF11 PBO 11
+ */
+public class GajiPegawai {
+
+    String nama, alamat;
+    int uangTransport, uangTunjangan, gajiPokok, totalGaji;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public int getUangTransport() {
+        return uangTransport;
+    }
+
+    public void setUangTransport(int uangTransport) {
+        this.uangTransport = uangTransport;
+    }
+
+    public int getUangTunjangan() {
+        return uangTunjangan;
+    }
+
+    public void setUangTunjangan(int uangTunjangan) {
+        this.uangTunjangan = uangTunjangan;
+    }
+
+    public int getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public void setGajiPokok(int gajiPokok) {
+        this.gajiPokok = gajiPokok;
+    }
+
+    public int getTotalGaji() {
+        return totalGaji;
+    }
+
+    public void setTotalGaji(int totalGaji) {
+        this.totalGaji = totalGaji;
+    }
+
+    public int gajiTotal(int uangTransport, int uangTunjangan, int gajiPokok) {
+        int gajiTotal = uangTransport + uangTunjangan + gajiPokok;
+        return gajiTotal;
+
+    }
+}
---
+++ src/if11_10116467_latihanuts2/IF11_10116467_LATIHANUTS2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if11_10116467_latihanuts2;
+
+/**
+ *
+ * @author Esa IF11 PBO 11
+ */
+public class IF11_10116467_LATIHANUTS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
